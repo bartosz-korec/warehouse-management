4ee94963c7cbef760dd33554e4ed135975774027
Bartosz Korec <118128567+bartosz-korec@users.noreply.github.com>
2025-08-25 23:10:21 +0200

    feat(auth): introduce JWT authentication

---
+++ backend/src/main/java/com/bartoszkorec/warehouse_management/config/ApplicationConfiguration.java
@@ -0,0 +1,44 @@
+package com.bartoszkorec.warehouse_management.config;
+
+import com.bartoszkorec.warehouse_management.exception.UserNotFoundException;
+import com.bartoszkorec.warehouse_management.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+public class ApplicationConfiguration {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UserNotFoundException("User with email " + username + " not found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
