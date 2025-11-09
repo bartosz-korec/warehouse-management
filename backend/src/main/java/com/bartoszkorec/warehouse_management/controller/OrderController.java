f50a76f7205d50a2c28d9bdf8c652759fecc5a08
bartosz-korec <118128567+bartosz-korec@users.noreply.github.com>
2025-11-09 20:44:44 +0100

    fix: id mapping

--- backend/src/main/java/com/bartoszkorec/warehouse_management/controller/OrderController.java
+++ backend/src/main/java/com/bartoszkorec/warehouse_management/controller/OrderController.java
@@ -1,15 +1,23 @@
 package com.bartoszkorec.warehouse_management.controller;
 
 import com.bartoszkorec.warehouse_management.annotation.HasRole;
+import com.bartoszkorec.warehouse_management.dto.LocationDto;
+import com.bartoszkorec.warehouse_management.dto.request.CalculateRouteRequest;
+import com.bartoszkorec.warehouse_management.model.Distance;
 import com.bartoszkorec.warehouse_management.model.Role;
 import com.bartoszkorec.warehouse_management.service.DistanceMatrixService;
 import com.bartoszkorec.warehouse_management.service.LocationService;
 import com.bartoszkorec.warehouse_management.service.OrderService;
 import com.bartoszkorec.warehouse_management.utils.LocationHelper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -27,5 +35,18 @@ public class OrderController {
                 LocationHelper.toDto(locationService.getStartingLocation().orElseThrow(
                 () -> new IllegalStateException("Starting location not found")
         )));
+    }
+
+    @PostMapping("calculate")
+    public String calculateRouteForLocationIds(@RequestBody @Valid CalculateRouteRequest request) {
+        Distance[][] matrix = distanceMatrixService.getDistanceMatrix().getMatrix();
+        LocationDto starting = LocationHelper.toDto(locationService.getStartingLocation().orElseThrow(
+                () -> new IllegalStateException("Starting location not found")
+        ));
+
+        Set<Integer> indices = new HashSet<>(request.locationIds());
+        indices.add(starting.id());
+
+        return orderService.calculateRouteForLocationIds(indices, matrix, starting);
     }
 }
